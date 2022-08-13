e9daa51350873132142ea6f6a139313a107e41d3
hudsonviana <hudson1206@gmail.com>
2022-08-13 11:15:27 -0300

    #01 - Listando e pegando evento de clique nos botões

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-console.log('first commit');
+const books = [
+    {
+        id: 1,
+        name: 'Book 1'
+    },
+    {
+        id: 2,
+        name: 'Book 2'
+    },
+    {
+        id: 3,
+        name: 'Book 3'
+    },
+    {
+        id: 4,
+        name: 'Book 4'
+    },
+];
+
+const booksElement = document.querySelector('#books');
+
+function render () {
+    let bookHTML = '<ul>';
+    books.forEach((objeto, index) => {
+        bookHTML += `
+            <li>${objeto.name} <button id="btnAddToCart" data-id="${objeto.id}">Add to Cart</button></li>
+        `;
+    });
+    bookHTML += '</ul>'
+
+    booksElement.innerHTML = bookHTML;
+}
+
+render();
+
+const btnsAddToCart = document.querySelectorAll('#btnAddToCart');
+
+btnsAddToCart.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        console.log(btn.getAttribute('data-id'));
+    });
+});
